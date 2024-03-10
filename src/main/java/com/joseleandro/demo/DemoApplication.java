9a0c6bb73eb99ad155114af0dbc227e4f6792648
Jleandrodc <leandro.frot@hotmail.com>
2024-03-09 21:02:54 -0300

    project created

---
+++ src/main/java/com/joseleandro/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.joseleandro.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //Notação para mostrar que a aplicação é Spring Boot
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joseleandro/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseleandro.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
